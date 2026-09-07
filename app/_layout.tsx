54a23159c1755e1c99af596f41862a01d419adf7
galindojustinvince16-png <galindo.justinvince16@gmail.com>
2026-09-07 16:02:36 +0800

    Login Social

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
+      <Stack.Screen name="register" />
       <Stack.Screen name="forgot-password" />
     </Stack>
   );
